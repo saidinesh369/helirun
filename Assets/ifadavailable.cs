f6f099d0c50987a2558e6f9c200b26ee5f7f47bf
saidinesh369 <saidinesh.komanduri@gmail.com>
2017-04-11 20:29:20 +0530

    v3

---
+++ Assets/ifadavailable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Advertisements;
+using UnityEngine;
+
+public class ifadavailable : MonoBehaviour {
+	public GameObject coins;
+	// Use this for initialization
+	void Start () {
+		if (Advertisement.IsReady ("rewardedVideo")) 
+			coins.SetActive (true);
+		else
+			coins.SetActive (false);
+			
+	}
+}
